2d5f780099a141ff75e05aa974f0e2ec0e55e586
masayasu oshiba <basirug45@gmail.com>
2017-04-25 01:31:57 +0000

    finish4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424054901) do
+ActiveRecord::Schema.define(version: 20170425012729) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "content"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170424054901) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "title"
+    t.string   "tasks"
   end
 
 end
